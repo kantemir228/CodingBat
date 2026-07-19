4ac683e9e81f59013fb48ac17aead7dc7ebbedd4
Kantemir <kantemirdzumakov@gmail.com>
2026-07-19 20:27:13 +0600

    no23

---
+++ src/main/java/Array/no23.java
@@ -0,0 +1,16 @@
+package Array;
+
+public class no23 {
+    public static void main(String[] args) {
+
+    }public boolean no23(int[] nums) {
+        for(int num:nums){
+            if(num==2||num==3){
+                return false;
+            }
+
+        }
+        return true;
+    }
+
+}
