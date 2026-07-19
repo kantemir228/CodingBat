a23be4b95571f6f81c7b8665580538a2f2ddd4e5
Kantemir <kantemirdzumakov@gmail.com>
2026-07-19 19:17:08 +0600

    stringE

---
+++ src/main/java/org/example/stringE.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class stringE {
+    public static void main(String[] args) {
+
+    }public boolean stringE(String str) {
+        int count=0;
+        for(int i=0; i<str.length();i++){
+            if(str.charAt(i)=='e'){
+                count++;
+            }
+        }return(count>=1&&count<=3);
+
+    }
+
+}
