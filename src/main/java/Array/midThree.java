bf6aef4a22e237341ccad4d0820070b5854d4b7a
Kantemir <kantemirdzumakov@gmail.com>
2026-07-19 21:08:36 +0600

    midThree

---
+++ src/main/java/Array/midThree.java
@@ -0,0 +1,14 @@
+package Array;
+
+public class midThree {
+    public static void main(String[] args) {
+
+    }public int[] midThree(int[] nums) {
+        int arr[]=new int[3];
+        arr[0]=nums[nums.length/2-1];
+        arr[1]=nums[nums.length/2];
+        arr[2]=nums[nums.length/2+1];
+        return arr;
+    }
+
+}
