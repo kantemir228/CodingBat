473929964966942a4e7a969df32803fdab836b8e
Kantemir <kantemirdzumakov@gmail.com>
2026-07-19 19:15:44 +0600

    in3050

---
+++ src/main/java/org/example/in3050.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class in3050 {
+    public static void main(String[] args) {
+
+    }public boolean in3050(int a, int b) {
+        if((a>=30&&a<=40)&&(b>=30&&b<=40)||(a>=40&&a<=50)&&(b>=40&&b<=50)){
+            return true;
+        }return false;
+    }
+
+}
