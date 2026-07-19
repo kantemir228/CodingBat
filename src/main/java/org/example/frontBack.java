60ef5ee8ac63ec687e67c82a66237b9213cd654a
Kantemir <kantemirdzumakov@gmail.com>
2026-07-19 16:43:57 +0600

    frontBack

---
+++ src/main/java/org/example/frontBack.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class frontBack {
+    public static void main(String[] args) {
+
+    }public String frontBack(String str) {
+        if(str.length()<=1){
+            return str;
+        }
+        return str.substring(str.length()-1)+str.substring(1,str.length()-1)+str.substring(0,1);
+    }
+
+}
