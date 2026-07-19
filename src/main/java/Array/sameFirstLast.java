3d1f7c67efdf9c7e8a9ff7ba43705caf39dc7108
Kantemir <kantemirdzumakov@gmail.com>
2026-07-19 20:18:17 +0600

    sameFirstLast

---
+++ src/main/java/Array/sameFirstLast.java
@@ -0,0 +1,12 @@
+package Array;
+
+public class sameFirstLast {
+    public static void main(String[] args) {
+
+    }public boolean sameFirstLast(int[] nums) {
+        if(nums.length>=1&&nums[0]==nums[nums.length-1]){
+            return true;
+        }return false;
+    }
+
+}
