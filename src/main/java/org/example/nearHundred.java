09f14c095410d2d36838508e8076845ebec4c40c
Kantemir <kantemirdzumakov@gmail.com>
2026-07-19 16:38:05 +0600

    nearHundred

--- src/main/java/org/example/nearHundred.java
+++ src/main/java/org/example/nearHundred.java
@@ -1,4 +1,12 @@
 package org.example;
 
 public class nearHundred {
+    public static void main(String[] args) {
+
+    }public boolean nearHundred(int n) {
+        if((n<=110&&n>=90)||(n<=210&&n>=190)){
+            return true;
+        }return false;
+    }
+
 }
