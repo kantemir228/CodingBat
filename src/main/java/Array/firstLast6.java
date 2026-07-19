6bedac6b323aebf893ef11c7d1fde817fb0978d4
Kantemir <kantemirdzumakov@gmail.com>
2026-07-19 20:15:30 +0600

    firstLast6

---
+++ src/main/java/Array/firstLast6.java
@@ -0,0 +1,12 @@
+package Array;
+
+public class firstLast6 {
+    public static void main(String[] args) {
+
+    }public boolean firstLast6(int[] nums) {
+        if(nums[0]==6||nums[nums.length-1]==6){
+            return true;
+        }return false;
+    }
+
+}
