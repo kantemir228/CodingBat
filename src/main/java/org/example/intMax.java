8eb8bed96f9dcd050396de8858f90f18ff871b56
Kantemir <kantemirdzumakov@gmail.com>
2026-07-19 19:14:01 +0600

    intMax

---
+++ src/main/java/org/example/intMax.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class intMax {
+    public static void main(String[] args) {
+
+    }public int intMax(int a, int b, int c) {
+        int max=a;
+        if(b>max){
+            max=b;
+        }if(c>max){
+            max=c;
+        }
+        return max;
+    }
+
+}
