b8b3562024e437c8b60c7af701919c7f020b55a9
Kantemir <kantemirdzumakov@gmail.com>
2026-07-19 19:17:53 +0600

    lastDigit

---
+++ src/main/java/org/example/lastDigit.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class lastDigit {
+    public static void main(String[] args) {
+
+    }
+    public boolean lastDigit(int a, int b) {
+        return a%10==b%10;
+    }
+
+}
