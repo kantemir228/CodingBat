fd3cbc0fa26783d62cd7e5519834d2f265b08901
Kantemir <kantemirdzumakov@gmail.com>
2026-07-19 16:41:51 +0600

    notString

--- src/main/java/org/example/notString.java
+++ src/main/java/org/example/notString.java
@@ -1,4 +1,15 @@
 package org.example;
 
 public class notString {
+    public static void main(String[] args) {
+
+    }public String notString(String str) {
+        if(str.startsWith("not")){
+            return str;
+        }else{
+            return "not "+str;
+        }
+    }
+
+
 }
