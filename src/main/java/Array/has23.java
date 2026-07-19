6de501c801708ffdffa69d9793697c25e22caab7
Kantemir <kantemirdzumakov@gmail.com>
2026-07-19 20:26:34 +0600

    has23

---
+++ src/main/java/Array/has23.java
@@ -0,0 +1,16 @@
+package Array;
+
+public class has23 {
+    public static void main(String[] args) {
+
+    }public boolean has23(int[] nums) {
+        for(int num:nums){
+            if(num==2||num==3){
+                return true;
+            }
+
+        }
+        return false;
+    }
+
+}
