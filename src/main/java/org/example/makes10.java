9a2c56aed8288bce08cf9d543f14d4a1e8c828c7
Kantemir <kantemirdzumakov@gmail.com>
2026-07-19 16:35:42 +0600

    makes10

---
+++ src/main/java/org/example/makes10.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class makes10 {
+    public static void main(String[] args) {
+
+    }public boolean makes10(int a, int b) {
+        if(a==10||b==10){
+            return true;
+        }else if(a+b==10){
+            return true;
+        }return false;
+    }
+
+}
