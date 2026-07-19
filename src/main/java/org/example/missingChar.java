2719f14cde8097ad9d0d5f799f1d7c5a1301e2aa
Kantemir <kantemirdzumakov@gmail.com>
2026-07-19 16:42:50 +0600

    missingChar

---
+++ src/main/java/org/example/missingChar.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class missingChar {
+    public static void main(String[] args) {
+
+    }public String missingChar(String str, int n) {
+        return str.substring(0,n)+str.substring(n+1);
+    }
+
+}
