82f0e9acf9f9f84e3c3285c35eff8f78b83e3289
Kantemir <kantemirdzumakov@gmail.com>
2026-07-19 16:11:52 +0600

    monkeyTrouble

--- src/main/java/org/example/monkeyTrouble.java
+++ src/main/java/org/example/monkeyTrouble.java
@@ -1,4 +1,19 @@
 package org.example;
 
 public class monkeyTrouble {
+    public static void main(String[] args) {
+
+    }
+    public boolean monkeyTrouble(boolean aSmile, boolean bSmile) {
+        if(aSmile==true&&aSmile==false){
+            return false;
+        }else if(aSmile==false&&bSmile==false){
+            return true;
+        } else if(aSmile==true&&bSmile==true){
+            return true;
+        }
+        return false;
+    }
+
+
 }
