4f63b4321f7abc61106774c11c73948eaf771913
Kantemir <kantemirdzumakov@gmail.com>
2026-07-19 20:25:19 +0600

    middleWay

---
+++ src/main/java/Array/middleWay.java
@@ -0,0 +1,21 @@
+package Array;
+
+public class middleWay {
+    public static void main(String[] args) {
+
+    }public int[] middleWay(int[] a, int[] b) {
+        int arr[]=new int[2];
+        int sr=0;
+        int sr1=0;
+
+        sr=a[1];
+
+
+        sr1=b[1];
+
+        arr[0]=sr;
+        arr[1]=sr1;
+        return arr;
+    }
+
+}
