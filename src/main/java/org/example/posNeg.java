d2dc407a8089b1031191b06f1fa1b41ce500bc34
Kantemir <kantemirdzumakov@gmail.com>
2026-07-19 16:39:42 +0600

    posNeg

--- src/main/java/org/example/posNeg.java
+++ src/main/java/org/example/posNeg.java
@@ -1,4 +1,22 @@
 package org.example;
 
 public class posNeg {
+    public static void main(String[] args) {
+
+    }public boolean posNeg(int a, int b, boolean negative) {
+        if((a>0&&b<0)&&negative==false){
+            return true;
+        } else if((a<0&&b>0)&&negative==false){
+            return true;
+        }else if((a>0&&b>0)&&negative==true){
+            return false;
+
+        }else if((a<0&&b<0)&&negative==true){
+            return true;
+
+        }else{
+            return false;
+        }
+    }
+
 }
