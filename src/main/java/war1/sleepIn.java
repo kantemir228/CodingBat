710c910560da965418fb1910400e9df0124f962b
Kantemir <kantemirdzumakov@gmail.com>
2026-07-19 15:48:30 +0600

    Initial commit

---
+++ src/main/java/war1/sleepIn.java
@@ -0,0 +1,18 @@
+package war1;
+
+public class sleepIn {
+    public static void main(String[] args) {
+
+    }public boolean sleepIn(boolean weekday, boolean vacation) {
+        if(weekday==true&&vacation==false){
+            return false;
+        }else if(weekday==false&&vacation==false){
+            return true;
+        } else if(weekday==false&&vacation==true){
+            return true;
+        }else if(weekday==true&&vacation==true){
+            return true;
+        }
+        return false;
+    }
+}
