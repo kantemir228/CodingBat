441bf6ac376a70d8694616c73f020438bf859eb0
Kantemir <kantemirdzumakov@gmail.com>
2026-07-19 16:47:40 +0600

    or35

---
+++ src/main/java/org/example/or35.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class or35 {
+    public static void main(String[] args) {
+
+    }public boolean or35(int n) {
+        if((n>0)&&n%3==0||n%5==0){
+            return true;
+        }return false;
+    }
+
+}
