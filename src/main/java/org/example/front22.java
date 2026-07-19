9488367f31f807a5b88a455d0e8e94fcf35cb5d3
Kantemir <kantemirdzumakov@gmail.com>
2026-07-19 19:03:35 +0600

    front22

---
+++ src/main/java/org/example/front22.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class front22 {
+    public static void main(String[] args) {
+
+    }public String front22(String str) {
+        if(str.length()>=3){
+            return str.substring(0,2)+str+str.substring(0,2);
+        }return str+str+str;
+    }
+
+
+}
