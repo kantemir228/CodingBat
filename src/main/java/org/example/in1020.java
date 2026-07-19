7ee63334698d65d1397e9e376c620b52643fa748
Kantemir <kantemirdzumakov@gmail.com>
2026-07-19 19:07:42 +0600

    in1020

---
+++ src/main/java/org/example/in1020.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class in1020 {
+    public static void main(String[] args) {
+
+    }public boolean in1020(int a, int b) {
+        if((10<=a&&a<=20)||(10<=b&&b<=20)){
+            return true;
+
+        }return false;
+    }
+
+}
