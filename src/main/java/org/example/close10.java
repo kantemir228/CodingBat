b953066dfd9cf14323ece061f48b47b1e3401108
Kantemir <kantemirdzumakov@gmail.com>
2026-07-19 19:14:46 +0600

    close10

---
+++ src/main/java/org/example/close10.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class close10 {
+    public static void main(String[] args) {
+
+    }public int close10(int a, int b) {
+        if(a>0&&b>0){
+            if((10-a)>(10-b)){
+                return a;
+            }if((10-b)>(10-a)){
+                return a;
+            }
+        }return 0;
+    }
+
+
+}
