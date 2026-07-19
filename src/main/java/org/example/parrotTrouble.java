5d64e671ed2062571c9ed73d0368266fed426b32
Kantemir <kantemirdzumakov@gmail.com>
2026-07-19 16:34:06 +0600

    parrotTrouble

--- src/main/java/org/example/parrotTrouble.java
+++ src/main/java/org/example/parrotTrouble.java
@@ -1,4 +1,16 @@
 package org.example;
 
 public class parrotTrouble {
+    public static void main(String[] args) {
+
+    }public boolean parrotTrouble(boolean talking, int hour) {
+        if(talking==true&&(hour<7||hour>20)){
+            return true;
+        }else if(talking==false&&hour<7){
+            return false;
+
+        }return false;
+
+    }
+
 }
