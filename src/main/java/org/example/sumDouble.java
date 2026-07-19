3c3de7c896b7b712eec25cc126351b4d74f973a4
Kantemir <kantemirdzumakov@gmail.com>
2026-07-19 16:16:48 +0600

    sumDouble

---
+++ src/main/java/org/example/sumDouble.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class sumDouble {
+    public static void main(String[] args) {
+
+    }
+    public int sumDouble(int a, int b) {
+        int sum=0;
+        if(a==b){
+            sum=(a+b)*2;
+        }else{
+            sum=a+b;
+        }
+        return sum;
+    }
+
+}
