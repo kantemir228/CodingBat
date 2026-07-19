36d0c0e2d9c0890e9f7211f441b9fd64bc18ea70
Kantemir <kantemirdzumakov@gmail.com>
2026-07-19 20:21:14 +0600

    sum3

---
+++ src/main/java/Array/sum3.java
@@ -0,0 +1,14 @@
+package Array;
+
+public class sum3 {
+    public static void main(String[] args) {
+
+    }public int sum3(int[] nums) {
+        int sum=0;
+        for(int num:nums){
+            sum+=num;
+        }
+        return sum;
+    }
+
+}
