f7e4d34310fae5ecfb85862ae3c18b76bdc5b735
Kantemir <kantemirdzumakov@gmail.com>
2026-07-19 21:06:57 +0600

    plusTwo

---
+++ src/main/java/Array/plusTwo.java
@@ -0,0 +1,15 @@
+package Array;
+
+public class plusTwo {
+    public static void main(String[] args) {
+
+    }public int[] plusTwo(int[] a, int[] b) {
+        int arr[]=new int[4];
+        arr[0]=a[0];
+        arr[1]=a[1];
+        arr[2]=b[0];
+        arr[3]=b[1];
+        return arr;
+    }
+
+}
