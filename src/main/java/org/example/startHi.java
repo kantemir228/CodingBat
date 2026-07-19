04e2a53ac5180709fd265e5742243821b98fafad
Kantemir <kantemirdzumakov@gmail.com>
2026-07-19 19:04:57 +0600

    startHi

---
+++ src/main/java/org/example/startHi.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class startHi {
+    public static void main(String[] args) {
+
+    }public boolean startHi(String str) {
+        if(str.startsWith("hi")){
+            return true;
+        }return false;
+    }
+
+}
