04c1ab8113933935ca99353e714ab9df3f0e5a5f
Kantemir <kantemirdzumakov@gmail.com>
2026-07-19 20:28:45 +0600

    double23

---
+++ src/main/java/Array/double23.java
@@ -0,0 +1,17 @@
+package Array;
+
+public class double23 {
+    public static void main(String[] args) {
+
+    }public boolean double23(int[] nums) {
+        if(nums.length>=2){
+
+
+            if((nums[0]==2&&nums[1]==2)||(nums[0]==3&&nums[1]==3)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
