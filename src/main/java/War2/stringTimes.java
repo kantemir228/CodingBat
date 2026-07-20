8d4e8ae770802c83d1354ab40f155b6e85248b94
Kantemir <kantemirdzumakov@gmail.com>
2026-07-20 08:29:58 +0600

    stringTimes

---
+++ src/main/java/War2/stringTimes.java
@@ -0,0 +1,14 @@
+package War2;
+
+public class stringTimes {
+    public static void main(String[] args) {
+
+    }public String stringTimes(String str, int n) {
+        String result = "";
+        for(int i=0;i<n;i++){
+            result += str;
+        }
+        return result;
+    }
+
+}
