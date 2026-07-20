139f20082c5931b3b85fd0f93992a52e5f4691a0
Kantemir <kantemirdzumakov@gmail.com>
2026-07-20 08:36:05 +0600

    arrayFront9

---
+++ src/main/java/War2/arrayFront9.java
@@ -0,0 +1,14 @@
+package War2;
+
+public class arrayFront9 {
+    public static void main(String[] args) {
+
+    }public boolean arrayFront9(int[] nums) {
+        for (int i = 0; i < nums.length && i < 4; i++) {
+            if (nums[i] == 9) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
