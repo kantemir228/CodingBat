4df795f36bb494aabb1f3287f4d2601e4912ca69
Kantemir <kantemirdzumakov@gmail.com>
2026-07-20 08:03:28 +0600

    comboString

---
+++ src/main/java/String/comboString.java
@@ -0,0 +1,14 @@
+package String;
+
+public class comboString {
+    public static void main(String[] args) {
+
+    }public String comboString(String a, String b) {
+        if (a.length()>b.length()){
+            return b + a +b;
+        }else  {
+            return  a + b +a;
+        }
+    }
+
+}
