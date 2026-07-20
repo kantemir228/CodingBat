5c8dabe00783706ac16b492d6ca1565b471deb3d
Kantemir <kantemirdzumakov@gmail.com>
2026-07-20 08:04:54 +0600

    left2

---
+++ src/main/java/String/left2.java
@@ -0,0 +1,10 @@
+package String;
+
+public class left2 {
+    public static void main(String[] args) {
+
+    }public String left2(String str) {
+        return str.substring(2).concat(str.substring(0,2));
+    }
+
+}
