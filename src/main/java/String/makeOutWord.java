06b00a47f018ae695399d62611fc667f7567c2be
Kantemir <kantemirdzumakov@gmail.com>
2026-07-20 07:58:55 +0600

    makeOutWorld

---
+++ src/main/java/String/makeOutWord.java
@@ -0,0 +1,10 @@
+package String;
+
+public class makeOutWord {
+    public static void main(String[] args) {
+
+    }public String makeOutWord(String out, String word) {
+        return out.substring(0,2)+word+out.substring(2);
+    }
+
+}
