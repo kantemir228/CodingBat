0a89a590b033ded4810330bfc8ca827131e6e213
Kantemir <kantemirdzumakov@gmail.com>
2026-07-20 08:32:07 +0600

    countXX

---
+++ src/main/java/War2/countXX.java
@@ -0,0 +1,14 @@
+package War2;
+
+public class countXX {
+    public static void main(String[] args) {
+
+    }int countXX(String str) {
+        int count=0;
+        for(int i =0;i<str.length()-1;i++){
+            if(str.substring(i,i+2).equals("xx")){
+                count++;
+            }
+        }return count;
+    }
+}
