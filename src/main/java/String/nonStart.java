d1a50f0ac9a9a8b1bf9c55f75f25384d6301f70b
Kantemir <kantemirdzumakov@gmail.com>
2026-07-20 08:04:15 +0600

    nonStart

---
+++ src/main/java/String/nonStart.java
@@ -0,0 +1,12 @@
+package String;
+
+public class nonStart {
+    public static void main(String[] args) {
+
+    }public String nonStart(String a, String b) {
+        String word =a.substring(1);
+        String word2=b.substring(1);
+        return word + word2;
+
+    }
+}
