c37754785dc31650a10c740f19b25e1f216df9da
Kantemir <kantemirdzumakov@gmail.com>
2026-07-20 08:15:39 +0600

    seeColor

---
+++ src/main/java/String/seeColor.java
@@ -0,0 +1,15 @@
+package String;
+
+public class seeColor {
+    public static void main(String[] args) {
+
+    }public String seeColor(String str) {
+        if(str.startsWith("red")){
+            return "red";
+
+        }else if(str.startsWith("blue")){
+            return "blue";
+
+        }return "";
+    }
+}
