8b7aa08489a8cbd218d3fb8e9b7f35a891affdab
Kantemir <kantemirdzumakov@gmail.com>
2026-07-20 07:55:41 +0600

    helloName

---
+++ src/main/java/String/helloName.java
@@ -0,0 +1,10 @@
+package String;
+
+public class helloName {
+    public static void main(String[] args) {
+
+    }public String helloName(String name) {
+        return "Hello".concat(" ").concat(name).concat("!");
+    }
+
+}
