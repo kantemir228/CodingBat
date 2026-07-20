e0de5228ea673013a61620efc925b9916969adf4
Kantemir <kantemirdzumakov@gmail.com>
2026-07-20 08:07:19 +0600

    withouEnd2

---
+++ src/main/java/String/withouEnd2.java
@@ -0,0 +1,13 @@
+package String;
+
+public class withouEnd2 {
+    public static void main(String[] args) {
+
+    }public String withouEnd2(String str) {
+        if(str.length()>2){
+            return str.substring(1,str.length()-1);
+
+        }return"";
+    }
+
+}
