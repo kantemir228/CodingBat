f0081c3f96f0cc5fb44576e7487693437e9d26c8
Kantemir <kantemirdzumakov@gmail.com>
2026-07-20 07:59:45 +0600

    extraEnd

---
+++ src/main/java/String/extraEnd.java
@@ -0,0 +1,11 @@
+package String;
+
+public class extraEnd {
+    public static void main(String[] args) {
+
+    }public String extraEnd(String str) {
+        String word = str.substring(str.length()-2);
+        return word+word+word;
+    }
+
+}
