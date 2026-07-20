260698661c86591a3c58c81944c71de883627436
Kantemir <kantemirdzumakov@gmail.com>
2026-07-20 07:57:56 +0600

    makeTags

---
+++ src/main/java/String/makeTags.java
@@ -0,0 +1,10 @@
+package String;
+
+public class makeTags {
+    public static void main(String[] args) {
+
+    }public String makeTags(String tag, String word) {
+        return "<"+tag+">"+word+"</"+tag+">";
+    }
+
+}
