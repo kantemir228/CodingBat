5ef4250b79d3ae82a5b84ed7e80417a4826afded
Kantemir <kantemirdzumakov@gmail.com>
2026-07-20 08:01:59 +0600

    withoutEnd

---
+++ src/main/java/String/withoutEnd.java
@@ -0,0 +1,10 @@
+package String;
+
+public class withoutEnd {
+    public static void main(String[] args) {
+
+    }public String withoutEnd(String str) {
+        return str.substring(1, str.length() - 1);
+    }
+
+}
