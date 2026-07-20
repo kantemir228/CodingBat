56bacde5fd50d11e58cacfa0d5e6fab5a7694e99
Kantemir <kantemirdzumakov@gmail.com>
2026-07-20 08:10:05 +0600

    twoChar

---
+++ src/main/java/String/twoChar.java
@@ -0,0 +1,15 @@
+package String;
+
+public class twoChar {
+    public static void main(String[] args) {
+
+    }public String twoChar(String str, int index) {
+        if(index<str.length()-1&&index >0){
+            return str.substring(index,index+2);
+
+        }else{
+            return str.substring(0,2);
+        }
+    }
+
+}
