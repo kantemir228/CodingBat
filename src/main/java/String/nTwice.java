134a0043520cb74b9bfcec668869c2ffdc7fd92d
Kantemir <kantemirdzumakov@gmail.com>
2026-07-20 08:09:22 +0600

    nTwice

---
+++ src/main/java/String/nTwice.java
@@ -0,0 +1,10 @@
+package String;
+
+public class nTwice {
+    public static void main(String[] args) {
+
+    }public String nTwice(String str, int n) {
+        return str.substring(0,n)+str.substring(str.length()-n);
+    }
+
+}
