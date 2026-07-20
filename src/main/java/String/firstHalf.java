8b67c933f7379a6b828509a549d30e0e4447e8c7
Kantemir <kantemirdzumakov@gmail.com>
2026-07-20 08:01:25 +0600

    firsHalf

---
+++ src/main/java/String/firstHalf.java
@@ -0,0 +1,10 @@
+package String;
+
+public class firstHalf {
+    public static void main(String[] args) {
+
+    }public String firstHalf(String str) {
+        return str.substring(0,str.length()/2);
+    }
+
+}
