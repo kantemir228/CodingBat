2f23a6be0e4ddb8e8ad7d8da209d8441ba62669f
Kantemir <kantemirdzumakov@gmail.com>
2026-07-20 07:57:09 +0600

    makeAbba

---
+++ src/main/java/String/makeAbba.java
@@ -0,0 +1,11 @@
+package String;
+
+public class makeAbba {
+    public static void main(String[] args) {
+
+    }public String makeAbba(String a, String b) {
+        return  a + b +b + a;
+
+    }
+
+}
