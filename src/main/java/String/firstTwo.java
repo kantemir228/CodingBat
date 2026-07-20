f62d4a101bd052c3bdd06ef977987dbb351d44b4
Kantemir <kantemirdzumakov@gmail.com>
2026-07-20 08:00:23 +0600

    firstTwo

---
+++ src/main/java/String/firstTwo.java
@@ -0,0 +1,14 @@
+package String;
+
+public class firstTwo {
+    public static void main(String[] args) {
+
+    }public String firstTwo(String str) {
+        if (str.length() > 2) {
+            return str.substring(0, 2);
+        } else {
+            return str;
+        }
+    }
+
+}
