9d9173ebfb50e28511efd93b9318dd7f636c89be
Kantemir <kantemirdzumakov@gmail.com>
2026-07-20 08:05:25 +0600

    right2

---
+++ src/main/java/String/right2.java
@@ -0,0 +1,10 @@
+package String;
+
+public class right2 {
+    public static void main(String[] args) {
+
+    }public String right2(String str) {
+        return str.substring(str.length()-2)+str.substring(0,str.length()-2);
+    }
+
+}
