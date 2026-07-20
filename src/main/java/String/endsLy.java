dc500966ebd0a1c5f2986e278252eaf21ea9a4c7
Kantemir <kantemirdzumakov@gmail.com>
2026-07-20 08:08:37 +0600

    endsLy

---
+++ src/main/java/String/endsLy.java
@@ -0,0 +1,14 @@
+package String;
+
+public class endsLy {
+    public static void main(String[] args) {
+
+    }public boolean endsLy(String str) {
+        if(str.endsWith("ly")){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+}
