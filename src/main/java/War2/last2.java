158a2d60c56218222d840bf4d4b05d161a95ef1c
Kantemir <kantemirdzumakov@gmail.com>
2026-07-20 08:34:49 +0600

    last2

---
+++ src/main/java/War2/last2.java
@@ -0,0 +1,25 @@
+package War2;
+
+public class last2 {
+    public static void main(String[] args) {
+
+    }public int last2(String str) {
+
+        if (str.length() < 2) {
+            return 0;
+        }
+
+        String end = str.substring(str.length() - 2);
+        int count = 0;
+
+        for (int i = 0; i < str.length() - 2; i++) {
+            if (str.substring(i, i + 2).equals(end)) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+
+}
